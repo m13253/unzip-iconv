f8deabf5aae1cdaf9dfc7c6c3979f81bdc2e4d35
Star Brilliant <m13253@hotmail.com>
2013-11-08 01:57:47 +0800

    Apply unzip60-alt-iconv-utf8.patch

--- unzip60/unzpriv.h
+++ unzip60/unzpriv.h
@@ -3008,7 +3008,7 @@ char    *GetLoadPath     OF((__GPRO));                              /* local */
          !(((islochdr) || (isuxatt)) && \
            ((hostver) == 25 || (hostver) == 26 || (hostver) == 40))) || \
         (hostnum) == FS_HPFS_ || \
-        ((hostnum) == FS_NTFS_ && (hostver) == 50)) { \
+        ((hostnum) == FS_NTFS_ /* && (hostver) == 50 */ )) { \
         _OEM_INTERN((string)); \
     } else { \
         _ISO_INTERN((string)); \
